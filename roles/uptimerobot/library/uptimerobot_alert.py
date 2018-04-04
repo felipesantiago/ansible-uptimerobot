d81f96092c8a167184cc03393cd45dfcd4f642be
Felipe Santiago <felipe@felipesantiago.org>
2018-04-03 22:39:23 -0400

    Add AlertContact management

--- roles/uptimerobot/library/uptimerobot_alert.py
+++ roles/uptimerobot/library/uptimerobot_alert.py
@@ -6,7 +6,9 @@ from ansible.module_utils.basic import AnsibleModule
 API_BASE = "https://api.uptimerobot.com/v2/"
 
 API_ACTIONS = dict(
-    new='newAlertContact'
+    new='newAlertContact',
+    getid='getAlertContacts',
+    delete='deleteAlertContact'
 )
 
 def main():
@@ -28,6 +30,24 @@ def main():
     alert_type = module.params['alert_type']
     value = module.params['value']
 
+    def getAlertContactID():
+        api_url = API_BASE + API_ACTIONS['getid']
+        payload = "api_key=" + api_key + "&format=json"
+
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'cache-control': "no-cache"
+        }
+
+        response = requests.request("POST", api_url, data=payload, headers=headers)
+        getid = json.loads(response.text)
+
+        contactlist = getid['total']
+
+        for getcontactid in range(contactlist):
+            if getid['alert_contacts'][getcontactid]['value'] == value:
+                return getid['alert_contacts'][getcontactid]['id']
+
     def newAlertContact():
 
         api_url = API_BASE + API_ACTIONS['new']
@@ -47,6 +67,29 @@ def main():
         else:
             return newalert['error']['message']
 
+    def deleteAlertContact():
+        api_url = API_BASE + API_ACTIONS['delete']
+
+        if getAlertContactID() is not None:
+            id = "%s" % getAlertContactID()
+        else:
+            return "Contact id not found"
+
+        payload = "api_key=" + api_key + "&format=json&id=" + id
+
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'cache-control': "no-cache"
+        }
+
+        response = requests.request("POST", api_url, data=payload, headers=headers)
+        result = json.loads(response.text)
+
+        if result['stat'] == "ok":
+            return "ok"
+        else:
+            return result['error']['message']
+
     if state == 'present':
         if newAlertContact() == "ok":
             module.exit_json(changed=True, name=alert_name, state=state)
@@ -55,9 +98,16 @@ def main():
         else:
             module.fail_json(msg= "%s" % newAlertContact())
 
+    elif state == "absent":
+        if deleteAlertContact() == "ok":
+            module.exit_json(changed=True, alert_name=alert_name, value=value, state=state)
+        elif deleteAlertContact() == "Contact id not found":
+            module.exit_json(msg="Not removed, reason: %s" % deleteAlertContact())
+        else:
+            module.fail_json(msg="%s" % deleteAlertContact())
+
     else:
         module.fail_json(msg="invalid state")
 
 if __name__ == '__main__':
     main()
-
